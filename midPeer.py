700a14d0b053a32b63ee4e01e2af4766fa4611d9
Xuancai Shaun <xuancaishaun@gmail.com>
2013-03-02 10:24:48 +0800

    Use UDP socket instead of RUDP

--- midPeer.py
+++ midPeer.py
@@ -1,20 +1,23 @@
-from rudp import *
-from rudpException import *
+#from rudp import *
+#from rudpException import *
+from socket import *
 
 class midPeer():
 	def __init__(self):
-		self.skt = rudpSocket(39951)
+		#self.skt = rudpSocket(39951)
+		self.skt = socket(AF_INET, SOCK_DGRAM)
+		self.skt.bind(('', 39951))
 		self.list = []
 
 	def wait(self):
 		# wait for the connection request from peerlist
 		# blocking
-		recvPkt, addr = self.skt.recvfrom()
+		recvPkt, addr = self.skt.recvfrom(100)
 		return addr
 
 	def introduce(self, peerA_addr, peerB_addr):
-		self.skt.sendto(str(peerA_addr), peerB_addr, True)
-		self.skt.sendto(str(peerB_addr), peerA_addr, True)
+		self.skt.sendto(str(peerA_addr), peerB_addr)
+		self.skt.sendto(str(peerB_addr), peerA_addr)
 
 	def start(self):
 		print 'midPeer is up; waiting for connection requests'
--- peer.py
+++ peer.py
@@ -1,20 +1,25 @@
-from rudp import *
-from rudpException import *
+#from rudp import *
+#from rudpException import *
+from socket import *
+from time import sleep, time
+from config import *
 
-midPeer_addr = ('137.165.1.114', 39951)
+midPeer_addr = ('200.0.206.203', 39951)
 
 class peer():
 	def __init__(self, port):
-		self.skt = rudpSocket(port)
+		#self.skt = rudpSocket(port)
+		self.skt = socket(AF_INET, SOCK_DGRAM) # uDP
+		self.skt.bind(('', port)) 
 
 	def connect(self, destAddr):
-		self.skt.sendto('connecting', destAddr, True)
+		self.skt.sendto('connecting', destAddr)
 		print 'peer connecting to midPeer'
 
 	def get(self):
 		# get target peer's IP address from midPeer
 		# blocking
-		recvPkt, addr = self.skt.recvfrom()
+		recvPkt, addr = self.skt.recvfrom(100)
 		# extract target peer address from recvPkt
 		ip_addr = recvPkt.split('(')[1].split(')')[0].split(',')[0][1:-1]
 		port = int(recvPkt.split('(')[1].split(')')[0].split(',')[1])
@@ -22,23 +27,23 @@ class peer():
 		return (ip_addr, port)
 
 	def touch(self, target_addr):
-		self.skt.sendto('sending', target_addr, False)
+		self.skt.sendto('sending', target_addr)
 		print 'peer punching a hole for', target_addr
 		
 		sleep(1)
-		self.skt.sendto('hello world', target_addr, False)
+		self.skt.sendto('hello world', target_addr)
 		print 'peer sending to target peer'
 
-		recvPkt, addr = self.skt.recvfrom()
+		recvPkt, addr = self.skt.recvfrom(100)
 		print 'peer has been contacted by target peer at', addr
 
 	def keepalive(self, target_addr):
 		while True:
 			sleep(1)
-			self.skt.sendto('hello', target_addr, False)
+			self.skt.sendto('hello', target_addr)
 			print 'peer has sent a packet @', str(int(time()))[-3:]
 
-			recvPkt, addr = self.skt.recvfrom()
+			recvPkt, addr = self.skt.recvfrom(100)
 			print 'peer has received a packet from ', addr
 
 
